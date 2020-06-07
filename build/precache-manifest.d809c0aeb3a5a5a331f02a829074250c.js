e3c41547919e77e886fad2cf8862e8358eb68c21
Muhammad Shariq Bilal <shariqghori01@gmail.com>
2020-06-07 16:45:41 -0700

    Copy Files into main Folder

---
+++ build/precache-manifest.d809c0aeb3a5a5a331f02a829074250c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e285a8e3fedf6ce03f866e0c46f6fd7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b3671fad01c9035aa3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22060da63afc0e324cfe",
+    "url": "/static/js/2.fac86350.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fac86350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b3671fad01c9035aa3",
+    "url": "/static/js/main.22f28871.chunk.js"
+  },
+  {
+    "revision": "398debaa7a15e56d2be5",
+    "url": "/static/js/runtime-main.6a97006d.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './piaic.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="piaic.png" alt="piaic" />
+        <h1>"Muhammad Shariq Bilal Ghori" (ASSIGNMENT 2)</h1>
+        <p>
+          This is My Second Assignment of Bootcamp2020 React App
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
